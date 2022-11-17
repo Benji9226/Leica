7a454d13f0247f00cdbfbd1b00d9ecd737785e42
PVNagel <philip.v.nagel@gmail.com>
2022-11-17 13:42:23 +0100

    Developed menu further

--- Leica/Leica/Program.cs
+++ Leica/Leica/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Leica.Application;
 using Leica.Domain;
+using Leica.UI;
 using System.Net.NetworkInformation;
 
 namespace Leica
@@ -21,82 +22,8 @@ namespace Leica
             repo.AddLeader(leader1);
             repo.AddLeader(leader2);
 
-            Console.WriteLine("Leica menu");
-            Console.WriteLine("1. Login");
-            Console.WriteLine("2. exit");
-            MenuChoice();
+            Menu menu = new Menu();
+            menu.MainMenu();
         }
-
-        static void MenuChoice()
-        {
-            int.TryParse(Console.ReadLine(), out int choice);
-            Console.Clear();
-            switch (choice)
-            {
-                case 1:
-                    bool login = false;
-                    while (login ==  false)
-                    {
-                        Console.Write("Enter Email: ");
-                        string emailInput = Console.ReadLine();
-                        Console.Write("\nEnter Password: ");
-                        int.TryParse(Console.ReadLine(), out int passwordInput);
-                        login = LoginCheck(emailInput, passwordInput);
-
-                        if (login)
-                        {
-                            Console.WriteLine("The worker exists. PRESS ENTER TO EXIT");
-                            Console.ReadLine();
-                            break;
-                        }
-
-                        Console.WriteLine("Wrong email or password, try again:");
-                        Console.ReadLine();
-                        Console.Clear();
-                    }
-                    break;
-                case 2:
-                    Console.WriteLine("Have a good day! :-) (press enter to shut down)");
-                    Console.ReadLine();
-                    Environment.Exit(0);
-                    break;
-                default:
-                    break;
-            }
-                
-        }
-
-        static bool LoginCheck(string email, int password)
-        {
-            if (File.Exists("Leaders.txt"))
-            {
-                using (StreamReader sr = new StreamReader("Leaders.txt"))
-                {
-                    string controlEmail = "";
-                    int controlPassword = 0;
-                    while (!sr.EndOfStream)
-                    {
-                        string tempString = sr.ReadLine();
-                        string[] tempArray = tempString.Split(';');
-                        controlEmail = tempArray[1];
-                        controlPassword = int.Parse(tempArray[2]);
-
-                        if (controlEmail == email && password == controlPassword)
-                        {
-                            return true;
-                        }
-                    }
-
-                    if (controlEmail != email || controlPassword != password)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return false;
-            
-        }
-        
     }
 }
---
+++ Leica/Leica/UI/Menu.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leica.UI
+{
+    public class Menu
+    {
+        public void MainMenu()
+        {
+            Console.WriteLine("Leica menu");
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. exit");
+
+            int.TryParse(Console.ReadLine(), out int choice);
+            Console.Clear();
+            switch (choice)
+            {
+                case 1:
+                    bool login = false;
+                    while (login == false)
+                    {
+                        Console.Write("Enter Email: ");
+                        string emailInput = Console.ReadLine();
+                        Console.Write("\nEnter Password: ");
+                        int.TryParse(Console.ReadLine(), out int passwordInput);
+                        login = LoginCheck(emailInput, passwordInput);
+
+                        if (login)
+                        {
+                            Console.WriteLine("The worker exists. PRESS ENTER TO EXIT");
+                            break;
+                        }
+
+                        Console.WriteLine("Wrong email or password, try again:");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Have a good day! :-) (press enter to shut down)");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void HRMenu()
+        {
+            Console.WriteLine("HELLO {insert name here} LIST OF WORKERS ASSIGNED TO YOU:");
+            Console.WriteLine();
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("1. P NAGEL - Software Department - philip.v.nagel");
+            Console.WriteLine("2. P NAGEL - Software Department - philip.v.nagel");
+        }
+
+        public bool LoginCheck(string email, int password)
+        {
+            if (File.Exists("Leaders.txt"))
+            {
+                using (StreamReader sr = new StreamReader("Leaders.txt"))
+                {
+                    string controlEmail = "";
+                    int controlPassword = 0;
+                    while (!sr.EndOfStream)
+                    {
+                        string tempString = sr.ReadLine();
+                        string[] tempArray = tempString.Split(';');
+                        controlEmail = tempArray[1];
+                        controlPassword = int.Parse(tempArray[2]);
+
+                        if (controlEmail == email && password == controlPassword)
+                        {
+                            return true;
+                        }
+                    }
+
+                    if (controlEmail != email || controlPassword != password)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return false;
+
+        }
+    }
+}
