059a83978a8dad03c9c2d092858e47eec893f224
Benjamin Telsing <Benjamin.telsing123@gmail.com>
2022-11-29 13:16:37 +0100

    Code Clean Up

--- Leica/Leica/Program.cs
+++ Leica/Leica/Program.cs
@@ -14,22 +14,12 @@ namespace Leica
 
         static void RunApp()
         {
-            //Hardcoded FILE MUST EXISTS WITH ATLEAST 1 USER
             LeaderRepo leaderRepo = new LeaderRepo();
             leaderRepo.InitializeRepository();
-
             EmployeeRepo employeeRepo = new EmployeeRepo();
-
             employeeRepo.InitializeRepository();
-            //employeeRepo.Add("Vuongvuongsen", "vuong@gmail.com", 555);
-            //employeeRepo.AddEmployeesToFile();
-            //Hardcoded end
-
-
-
             Menu menu = new Menu();
             menu.MainMenu();
-
         }
     }
 }
